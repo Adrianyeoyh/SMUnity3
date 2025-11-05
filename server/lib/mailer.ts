5bc640552c0d97600e11e3d88c405420ac10e892
adrian <adrianallinone@gmail.com>
2025-11-05 18:56:32 +0800

    Revert "SMTP secure"
    This reverts commit b0fae20c0f16aa78d59845764589375ae04c33c3.
    
    asdf#

--- server/lib/mailer.ts
+++ server/lib/mailer.ts
@@ -5,8 +5,8 @@ import { env } from "#server/env";
 export const mailer = nodemailer.createTransport({
   host: env.SMTP_HOST,
   port: env.SMTP_PORT,
-  secure:
-  env.SMTP_SECURE,
+  secure: false,
+  // env.SMTP_SECURE,
   auth: {
     user: env.SMTP_USER,
     pass: env.SMTP_PASS,
